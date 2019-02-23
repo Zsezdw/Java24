b4b46f184a3a2df7172bfab6a97289c999482521
Julia <Julia@Juszczak.info>
2019-02-23 12:20:01 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) // ctrl + d to kopiowanie linijki {
+        //deklaracja i incjalizacja danych
+        int x;
+        x =  100;
+
+        int y = 8;
+        double d = 5,6;
+
+        char c = "a";
+        int symbol = c;
+        system.out.println("symbol = " + symbol);
+        system.out.println ("c = " + c);
+
+        String str = "Ala ma kota";
+
+        boolean b = true;
+        boolean bl = false;
+
+    }
+}
